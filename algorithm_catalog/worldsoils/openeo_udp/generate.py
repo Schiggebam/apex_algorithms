0a6c7418a0b749462fa243c9022341fc48e26d3e
karl_pa <paul.karlshoefer@dlr.de>
2025-11-14 14:27:45 +0100

    src src_std with fixed th

--- algorithm_catalog/worldsoils/openeo_udp/generate.py
+++ algorithm_catalog/worldsoils/openeo_udp/generate.py
@@ -144,7 +144,7 @@ def test_run():
 
 
 if __name__ == "__main__":
-    if True:
+    if False:
         test_run()
         exit()
     # save process to json
--- algorithm_catalog/worldsoils/openeo_udp/scmap_composite_udf.py
+++ algorithm_catalog/worldsoils/openeo_udp/scmap_composite_udf.py
@@ -18,6 +18,14 @@ def _nmad(xarr: xr.DataArray) -> xr.DataArray:
     return xr.where(cond, np.nan, xarr)
 
 
+def _pvir2(xarr: xr.DataArray) -> xr.DataArray:
+    b04 = xarr.sel(bands='B02')
+    b08 = xarr.sel(bands='B08')
+    b12 = xarr.sel(bands='B12')
+
+    return ((b08 - b04) / (b08 + b04)) + ((b08 - b12) / (b08 + b12))
+
+
 def generate_composite(xarr: xr.DataArray, value) -> XarrayDataCube:
     """
     Lorem Ipsum
@@ -25,9 +33,27 @@ def generate_composite(xarr: xr.DataArray, value) -> XarrayDataCube:
     :param value: abc
     :return: Datacube 
     """
-    print(xarr)
-     
-    result = xarr.mean(dim="t", skipna=True)    # keepdims=False
+
+    b_bands = [b for b in xarr.band.values if b.startswith("B") and len(b) == 3]
+    xarr_reflectance = xarr.sel(band=b_bands)
+
+    th = .2
+    idx = _pvir2(xarr_reflectance)
+    cond_idx = (idx > th).broadcast_like(xarr_reflectance)
+
+    xarr_reflectance = xr.where(cond_idx, np.nan, xarr)
+
+    result_src      = xarr_reflectance.mean(dim="t", skipna=True)    # keepdims=False
+    result_src_std  = xarr_reflectance.std(dim="t", skipna=True)
+
+    result_src = result_src.assign_coords({
+        "bands": [f"SRC_{b}" for b in result_src["bands"].values]
+    })
+    result_src_std = result_src_std.assign_coords({
+        "bands": [f"SRC_STD_{b}" for b in result_src_std["bands"].values]
+    })
+
+    result = xr.concat([result_src, result_src_std], dim="bands")
     return XarrayDataCube(result)
 
 def apply_datacube(cube: XarrayDataCube, context: Dict) -> XarrayDataCube:
